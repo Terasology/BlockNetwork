bfde966f7f40321b7ca4589c48248bd37886f332
jasonyonathan <jasonyonathan@live.com>
2018-01-01 20:42:00 +0700

    Fixed javadoc

--- src/main/java/org/terasology/blockNetwork/BlockNetworkUtil.java
+++ src/main/java/org/terasology/blockNetwork/BlockNetworkUtil.java
@@ -30,7 +30,10 @@ public final class BlockNetworkUtil {
     * getResultSide
     * @param block - the block
     * @param definedSide - the definedSide
-    * @return a rotation of the definedSide
+    * @return a side rotation of the definedSide
+    * This method's function is to get the side of a block that is rotated somehow. The rotation of the block
+    * is obtained from the getRotation Method ({@link #getRotation}) and then to get the Side data, the rotation data
+    * is rotated to the defined side
     */
     public static Side getResultSide(Block block, Side definedSide) {
         Rotation rotation = getRotation(block);
@@ -41,6 +44,8 @@ public final class BlockNetworkUtil {
     * getRotation
     * @param block - the block
     * @return Rotation data of a block that used by getResultSide, getResultConnections, and getSourceConnections methods {@link #getResultSide, @link #getResultConnections, @link getSourceConnections}
+    * This Rotation method refers to the RotationBlockFamily class to get the rotation of a block. If the blockfamily of the block has RotationBlockFamily in instance,
+    * The method will return the rotation of the block. if not, it will return null
     */
     private static Rotation getRotation(Block block) {
         Rotation rotation = Rotation.none();
@@ -56,6 +61,8 @@ public final class BlockNetworkUtil {
     * @param block - the block
     * @param definedSides - the definedSides
     * @return a byte contains the amount of result connections of the block
+    * This method is used to count the result connection of the block. the iteration of rotating the block in definedSides will increase the amount
+    * of the connections
     */
     public static byte getResultConnections(Block block, byte definedSides) {
         Rotation rotation = getRotation(block);
@@ -73,6 +80,9 @@ public final class BlockNetworkUtil {
     * @param block - the block
     * @param connections - the connections
     * @return a byte contains the amount of source connections of the block
+    * This getSourceConnections method works almost similar to the getResultConnections method above. but, there's a different in the rotation var.
+    * The rotation var in this method finds the reverse of the block rotation of a defined block. There is also a different at the loop in this method.
+    * This method loops the sides of the connections defined as a parameter while the getResultConnections iterates from the definedSides.
     */
     public static byte getSourceConnections(Block block, byte connections) {
         Rotation rotation = getRotation(block);
