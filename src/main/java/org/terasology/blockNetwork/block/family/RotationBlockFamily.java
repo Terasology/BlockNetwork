662db77fedf780871c5824c1ab03605cba2a5b31
Niruandaleth <jd.rueckert@googlemail.com>
2020-05-17 21:53:01 +0200

    feat: add required blockfamily from `MarcinScIncubator`
    - remove `MarcinScIncubator` dependency

---
+++ src/main/java/org/terasology/blockNetwork/block/family/RotationBlockFamily.java
@@ -0,0 +1,39 @@
+/*
+ * Copyright 2020 MovingBlocks
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.terasology.blockNetwork.block.family;
+
+import org.terasology.math.Rotation;
+import org.terasology.math.Side;
+import org.terasology.world.block.Block;
+
+/**
+ * Block family interface that defined its blocks by rotation.
+ */
+public interface RotationBlockFamily {
+    /**
+     * Returns block from the block family for the specified rotation.
+     * @param rotation
+     * @return
+     */
+    Block getBlockForRotation(Rotation rotation);
+
+    /**
+     * Returns rotation used to create the specified block.
+     * @param block
+     * @return
+     */
+    Rotation getRotation(Block block);
+}
